ec2a6f3dec7672101af641ef3bbfa1a869a756b8
Ptitjbus <ptitjbus02@gmail.com>
2025-01-13 04:25:15 +0100

    add lyon 7 bar hero

--- pages/bars/lyon-7.vue
+++ pages/bars/lyon-7.vue
@@ -1,3 +1,14 @@
 <template>
-    
+    <div class="bg-black" :style="isMobile ? 'padding-top: 5rem;' : 'padding-top: 2rem;'">
+        <BarPageHero title="Lyon 7" address="135 Rue Sébastien Gryphe, 69007 Lyon" cta-label="Voir carte" cta-url="/carte.pdf" />
+    </div>
 </template>
+
+<script>
+import BarPageHero from '~/components/BarsPage/BarPageHero.vue';
+export default {
+  name:'BarsPage',
+  components: { BarPageHero },
+}
+</script>
+  
